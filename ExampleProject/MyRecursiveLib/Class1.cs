cb1c16926c236d1f8d2716fa44e25f18fc68c328
Aaron Burke <aaburke@microsoft.com>
2022-05-24 14:05:46 -0700

    add example project

---
+++ ExampleProject/MyRecursiveLib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MyRecursiveLib
+{
+    public class Class1
+    {
+    }
+}
---
+++ ExampleProject/UnusedLibrary/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace UnusedLibrary
+{
+    public class Class1
+    {
+    }
+}
